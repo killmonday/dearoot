eb11ad550bdff89955b840d831972bbd315326d6
killmonday <59520081+killmonday@users.noreply.github.com>
2022-10-05 15:40:39 +0800

    Add files via upload

---
+++ dearoot/userland/include/config.h
@@ -0,0 +1,16 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define HOMEDIR		"/root"
+#define GET_FILE 	1
+#define PUT_FILE 	2
+#define RUNSHELL 	3
+#define SET_DELAY 	4
+#define OUT 		5
+#define RUNCMD_NO_OUTPUT 		6
+#define EXIT_LEN 	16
+#define EXIT 		";7(Zu9YTsA7qQ#vw"
+
+#define SHELL_DIR "/"HIDE
+
+#endif
---
+++ dearoot/userland/include/util.h
@@ -0,0 +1,60 @@
+#ifndef _UTIL_H
+#define _UTIL_H
+
+//#include "config.h"
+
+#define ERROR -1
+#define RL_BUFSIZE 2048
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " \t\r\n\a"
+
+extern char *optarg;
+// extern int optind;
+
+char good[] = "\e[01;34m[*]\e[00m";
+char awesome[] = "\e[01;32m[+]\e[00m";
+char bad[] = "\e[01;31m[-]\e[00m";
+char warn[] = "\e[01;33m[!]\e[00m";
+
+void p_error(char *message)
+{
+	char error_message[129];
+
+	strcpy(error_message, bad);
+	strcat(error_message, " Error ");
+	strncat(error_message, message, 93);
+	perror(error_message);
+	printf("\n\n");
+}
+
+void fatal(char *message)
+{
+	p_error(message);
+	exit(ERROR);
+}
+
+void banner(void)
+{
+	fprintf(stdout, "\e[01;31m\n\n"
+"  ,-.       _,---._ __  / \\\n"
+" /  )    .-'       `./ /   \\\n"
+"(  (   ,'            `/    /|\n"
+" \\  `-\"             \\'\\   / |\n"
+"  `.              ,  \\ \\ /  |\n"
+"   /`.          ,'-`----Y   |\n"
+"  (            ;        |   '\n"
+"  |  ,-.    ,-'         |  /\n"
+"  |  | (   |    Dearoot | /\n"
+"  )  |  \\  `.___________|/\n"
+"  `--'   `--'"
+"\n\n");
+}
+
+void banner2(void)
+{
+	fprintf(stdout, "\e[01;31m\n\n"
+"ok"
+"\n");
+}
+
+#endif
